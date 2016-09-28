1137198f2e505ab644c2c9f3b6a70629a576c131
Sriram Sami <frizensami@gmail.com>
2016-09-28 14:26:49 +0800

    Add favorite command

--- src/seedu/addressbook/commands/HelpCommand.java
+++ src/seedu/addressbook/commands/HelpCommand.java
@@ -20,6 +20,7 @@ public class HelpCommand extends Command {
             + "\n" + ViewCommand.MESSAGE_USAGE
             + "\n" + ViewAllCommand.MESSAGE_USAGE
             + "\n" + HelpCommand.MESSAGE_USAGE
+            + "\n" + FavoriteCommand.MESSAGE_USAGE
             + "\n" + ExitCommand.MESSAGE_USAGE;
 
     public HelpCommand() {}
--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -15,6 +15,8 @@ public class Person implements ReadOnlyPerson {
     private Email email;
     private Address address;
 
+    private boolean isFavorite = false;
+
     private final UniqueTagList tags;
     /**
      * Assumption: Every field must be present and not null.
@@ -32,6 +34,16 @@ public class Person implements ReadOnlyPerson {
      */
     public Person(ReadOnlyPerson source) {
         this(source.getName(), source.getPhone(), source.getEmail(), source.getAddress(), source.getTags());
+    }
+
+    @Override
+    public boolean getIsFavorite() {
+        return favorite;
+    }
+
+    @Override
+    public void setIsFavorite(boolean isFavorite) {
+        this.isFavorite = isFavorite;
     }
 
     @Override
